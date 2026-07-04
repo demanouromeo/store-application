5db3d605abb60bac450459b9b63a502c6b8920cd
Romeo GABIN <gabinprof1@gmail.com>
2026-07-03 22:17:50 -0400

    1.1.2 - applicaction.yaml changed

--- src/main/java/com/dmsacad/store/configuration/StripeConfig.java
+++ src/main/java/com/dmsacad/store/configuration/StripeConfig.java
@@ -1,28 +1,23 @@
 package com.dmsacad.store.configuration;
 
-import com.stripe.Stripe;
-import io.jsonwebtoken.security.Keys;
-import jakarta.annotation.PostConstruct;
-import jakarta.validation.Valid;
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import javax.crypto.SecretKey;
+import com.stripe.Stripe;
+
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
 
 @Configuration
 //@ConfigurationProperties(prefix = "stripe")
 @Data
 public class StripeConfig {
+
     @Value("${stripe.secretKey}")
     private String secretKey;
 
-    // @Value("${stripe.publicKey}")
-    // private String publicKey;
-
     @PostConstruct//Tells Spring to call this method when this bean is created
-    public void init(){
+    public void init() {
         Stripe.apiKey = secretKey;
     }
 }
